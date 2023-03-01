a7d43ff27125e87abb9fc5ab500ef29ff2a0e6ab
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2023-03-01 20:53:09 +0100

    local storage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout
+    view="lHh Lpr lFf"
+    @keyup.ctrl.enter="showResetButton = !showResetButton"
+  >
     <q-page-container class="q-pa-md">
-      <h1 class="text-center q-my-xs primary">{{ timer }}</h1>
-      <q-dialog v-model="showInputDialog" >
+      <h1 class="text-center q-my-xs primary">
+        {{ Math.floor(timer / 60) }}:{{ timer % 60 }}
+      </h1>
+      <q-dialog v-model="showInputDialog">
         <q-card style="width: 700px; max-width: 80vw">
           <q-card-section>
             <q-input
@@ -37,10 +42,15 @@
       </div>
       <div v-if="myNumber && answer.join('') == myNumber">
         <div>Dobrze!</div>
-        <q-btn color="primary" class="q-mx-auto full-width" @click="reset">
-          Reset
-        </q-btn>
       </div>
+      <q-btn
+        v-if="showResetButton"
+        color="primary"
+        class="q-mx-auto full-width"
+        @click="reset"
+      >
+        Reset
+      </q-btn>
       <q-btn color="primary" class="full-width" @click="showInputDialog = true">
         Start
       </q-btn>
@@ -49,13 +59,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch } from "vue";
+import { ref, computed, watch, onMounted } from "vue";
 
 const myNumber = ref<string>("");
 const answer = ref<string[]>([]);
 const timer = ref<number>(60);
 const intervalId = ref<number>(0);
 const showInputDialog = ref<boolean>(false);
+const showResetButton = ref<boolean>(false);
 const timeInput = ref<number>(60);
 
 const splitedNumber = computed(() => myNumber.value.split(""));
@@ -77,27 +88,36 @@ const inputStyle = (value: string, index: number) => ({
   width: "3rem",
 });
 
+const startInterval = () => {
+  intervalId.value = setInterval(() => {
+    // TODO: Zamiast twgo wrzucić cały time do zmiennej i odczytywać różnice. Wtedy będzie działało nawet po wyłączeniu przeglądarki
+    localStorage.setItem("timer", --timer.value + "");
+  }, 1000);
+};
+
 const startGame = () => {
   if (intervalId.value > 0) {
     return;
   }
 
   timer.value = timeInput.value;
-  intervalId.value = setInterval(() => {
-    --timer.value;
-  }, 1000);
+  localStorage.setItem("myNumber", myNumber.value);
+  localStorage.setItem("timeInput", timeInput.value + "");
+  startInterval();
 };
 
 const reset = () => {
   myNumber.value = "";
   answer.value = [];
   clearInterval(intervalId.value);
+  showResetButton.value = false;
   intervalId.value = 0;
 };
 
 watch(timer, (val) => {
   if (val <= 0) {
     clearInterval(intervalId.value);
+    localStorage.clear();
   }
 });
 
@@ -110,4 +130,17 @@ watch(
   },
   { deep: true }
 );
+
+onMounted(() => {
+  const storedTimer = localStorage.getItem("timer");
+  const storagedMyNymber = localStorage.getItem("myNumber");
+  const storagedTimeInput = localStorage.getItem("timeInput");
+  if (storedTimer && storagedMyNymber && storagedTimeInput) {
+    timer.value = +storedTimer;
+    myNumber.value = storagedMyNymber;
+    timeInput.value = +storagedTimeInput;
+
+    startInterval();
+  }
+});
 </script>
