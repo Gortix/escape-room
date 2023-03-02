e42522bd42b94ec06a333f52db64ea86e0c0735a
Paweł Piechowiak <pawelpiecho1992@o2.pl>
2023-03-02 20:51:15 +0100

    small fixes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   >
     <q-page-container class="q-pa-md">
       <h1 class="text-center q-my-xs primary">
-        {{ Math.floor(timer / 60) }}:{{ timer % 60 }}
+        {{ displayTime }}
       </h1>
       <q-dialog v-model="showInputDialog">
         <q-card style="width: 700px; max-width: 80vw">
@@ -14,6 +14,7 @@
               v-model="myNumberInput"
               type="number"
               label="Wprowadź swoją cyfrę"
+              :rules="[(val) => !!val || 'Field is required']"
             />
             <q-input v-model="timeInput" type="number" label="Czas (s)" />
           </q-card-section>
@@ -33,16 +34,21 @@
           v-for="(value, index) in splitedNumber"
           :key="value + index"
           v-model="answer[index]"
-          :color="value == answer[index] ? 'green' : 'red'"
           outlined
           :style="inputStyle(value, index)"
           maxlength="1"
           class="col-1 q-ma-md p-ma-xs"
+          :readonly="!runed"
         />
       </div>
-      <div v-if="myNumber && answer.join('') == myNumber">
-        <div>Dobrze!</div>
-      </div>
+      <h2
+        v-if="myNumber && answer.join('') == myNumber"
+        style="text-align: center; color: green"
+      >
+        GRATULACJE!
+      </h2>
+      <h2 v-if="timeOver" style="text-align: center; color: red">KONIEC GRY</h2>
+
       <q-btn
         v-if="showResetButton"
         color="primary"
@@ -51,7 +57,12 @@
       >
         Reset
       </q-btn>
-      <q-btn color="primary" class="full-width" @click="showInputDialog = true">
+      <q-btn
+        v-if="!hideStart"
+        color="primary"
+        class="full-width"
+        @click="showInputDialog = true"
+      >
         Start
       </q-btn>
     </q-page-container>
@@ -61,6 +72,9 @@
 <script setup lang="ts">
 import { ref, computed, watch, onMounted } from "vue";
 
+const milisecondsToSeconds = (miliseconds: number) =>
+  Math.floor(miliseconds / 1000);
+
 const myNumber = ref<string>("");
 const answer = ref<string[]>([]);
 const timer = ref<number>(60);
@@ -68,6 +82,9 @@ const intervalId = ref<number>(0);
 const showInputDialog = ref<boolean>(false);
 const showResetButton = ref<boolean>(false);
 const timeInput = ref<number>(60);
+const runed = ref<boolean>(false);
+const timeOver = ref<boolean>(false);
+const hideStart = ref<boolean>(false);
 
 const splitedNumber = computed(() => myNumber.value.split(""));
 const myNumberInput = computed({
@@ -78,20 +95,34 @@ const myNumberInput = computed({
     }
   },
   get() {
-    return myNumber.value;
+    return myNumber.value || "0";
   },
 });
 
+const twoSigns = (nm: number) => (nm > 9 ? nm + "" : "0" + nm);
+
+const displayTime = computed(() => {
+  const minutes = Math.floor(timer.value / 60);
+  const secunds = timer.value % 60;
+
+  return twoSigns(minutes) + ":" + twoSigns(secunds);
+});
+
 const inputStyle = (value: string, index: number) => ({
-  backgroundColor: value == answer.value[index] ? "lightgreen" : "red",
+  // backgroundColor: value == answer.value[index] ? "lightgreen" : "red",
   fontSize: "2rem",
   width: "3rem",
 });
 
-const startInterval = () => {
+const startInterval = (startTime: number) => {
+  runed.value = true;
+  hideStart.value = true;
+  timer.value = milisecondsToSeconds(
+    startTime - Date.now() + timeInput.value * 1000
+  );
+
   intervalId.value = setInterval(() => {
-    // TODO: Zamiast twgo wrzucić cały time do zmiennej i odczytywać różnice. Wtedy będzie działało nawet po wyłączeniu przeglądarki
-    localStorage.setItem("timer", --timer.value + "");
+    --timer.value;
   }, 1000);
 };
 
@@ -100,15 +131,22 @@ const startGame = () => {
     return;
   }
 
+  const startTime = Date.now();
   timer.value = timeInput.value;
+
   localStorage.setItem("myNumber", myNumber.value);
   localStorage.setItem("timeInput", timeInput.value + "");
-  startInterval();
+  localStorage.setItem("startTime", startTime + "");
+
+  startInterval(startTime);
 };
 
 const reset = () => {
   myNumber.value = "";
   answer.value = [];
+  runed.value = false;
+  hideStart.value = false;
+  timeOver.value = false;
   clearInterval(intervalId.value);
   showResetButton.value = false;
   intervalId.value = 0;
@@ -117,7 +155,9 @@ const reset = () => {
 watch(timer, (val) => {
   if (val <= 0) {
     clearInterval(intervalId.value);
-    localStorage.clear();
+    // localStorage.clear();
+    runed.value = false;
+    timeOver.value = true;
   }
 });
 
@@ -126,21 +166,22 @@ watch(
   (val) => {
     if (val.join("") == myNumber.value) {
       clearInterval(intervalId.value);
+      runed.value = false;
     }
   },
   { deep: true }
 );
 
 onMounted(() => {
-  const storedTimer = localStorage.getItem("timer");
+  const storedStartTime = localStorage.getItem("startTime");
   const storagedMyNymber = localStorage.getItem("myNumber");
   const storagedTimeInput = localStorage.getItem("timeInput");
-  if (storedTimer && storagedMyNymber && storagedTimeInput) {
-    timer.value = +storedTimer;
+
+  if (storedStartTime && storagedMyNymber && storagedTimeInput) {
     myNumber.value = storagedMyNymber;
     timeInput.value = +storagedTimeInput;
 
-    startInterval();
+    startInterval(+storedStartTime);
   }
 });
 </script>
